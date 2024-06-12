d26c26ad6a53e0cff399722f9fd2d2ae5ffbdaa5
Lucas Silva <lucas2000bss@gmail.com>
2024-06-12 11:47:10 -0300

    Create Baner_whois.py

---
+++ Baner_whois.py
@@ -0,0 +1,84 @@
+import os
+import random
+
+random_numbers = random.randint(1,4)
+
+
+def MudarBaner(num=random_numbers):
+        os.system("clear")
+
+        if num == 1:
+                print("""\033[1;32m
+
+
+
+ _       ____          ____
+| |     / / /_  ____  /  _/____
+| | /| / / __ \/ __ \ / // ___/
+| |/ |/ / / / / /_/ // /(__  )
+|__/|__/_/ /_/\____/___/____/
+
+                 V. 1.0
+                [*] Developed by: Lucas-Dk
+                [*] Domínios de Sites
+                [*] Meu Insta: lbss_x
+
+
+             \033[m""")
+
+        elif num == 2:
+                print("""\033[1;34m
+
+
+
+ _       ____          ____
+| |     / / /_  ____  /  _/____
+| | /| / / __ \/ __ \ / // ___/
+| |/ |/ / / / / /_/ // /(__  )
+|__/|__/_/ /_/\____/___/____/
+
+                V. 1.0
+                [*] Developed by: Lucas-Dk
+                [*] Domínios de Sites
+                [*] Meu Insta: lbss_x
+
+
+             \033[m""")
+
+        elif num == 3:
+                print("""\033[1;31m
+
+
+
+ _       ____          ____
+| |     / / /_  ____  /  _/____
+| | /| / / __ \/ __ \ / // ___/
+| |/ |/ / / / / /_/ // /(__  )
+|__/|__/_/ /_/\____/___/____/
+
+                V. 1.0
+                [*] Developed by: Lucas-Dk
+                [*] Domínios de Sites
+                [*] Meu Insta: lbss_x
+
+
+             \033[m""")
+
+        elif num == 4:
+                print("""\033[1;35m
+
+
+
+ _       ____          ____
+| |     / / /_  ____  /  _/____
+| | /| / / __ \/ __ \ / // ___/
+| |/ |/ / / / / /_/ // /(__  )
+|__/|__/_/ /_/\____/___/____/
+
+                V. 1.0
+                [*] Developed by: Lucas-Dk
+                [*] Domínios de Sites
+                [*] Meu Insta: lbss_x
+
+
+             \033[m""")
